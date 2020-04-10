043d4521621715ce2cf239fed7eabd2f6b4f3025
hllinc <hllinc@163.com>
2020-04-10 23:21:01 +0800

    添加eslint支持。

--- src/mock.js
+++ src/mock.js
@@ -3,9 +3,9 @@
  * @author hongliang7
  * @date 2020-04-10 17:53
  */
-import Mock from 'mockjs'
+import Mock from 'mockjs';
 
-const domain = '/api/'
+const domain = '/api/';
 
 // 模拟getData接口
 Mock.mock(domain + 'getData', function () {
@@ -13,6 +13,6 @@ Mock.mock(domain + 'getData', function () {
         code: '200',
         message: 'OK',
         data: 'test'
-    }
-    return result
-})
+    };
+    return result;
+});
