8cd78ce4ae89013e08fd0617dedcf39c2597c6fd
hongliang7 <hongliang5@jd.com>
2020-04-29 19:05:41 +0800

    修改eslint配置。

--- src/router/demoComponent.tsx
+++ src/router/demoComponent.tsx
@@ -1,20 +1,20 @@
 /* 这个demo用于演示子路由，可以删除 */
-import React from 'react'
-import { routeProps } from '@/types/route'
-import { RenderRoutes } from '@/router/RenderRoutes'
+import React from 'react';
+import { routeProps } from '@/types/route';
+import { RenderRoutes } from '@/router/RenderRoutes';
 
 export const RouteDemoA = (routeProps: routeProps) => {
-  const { routes } = routeProps
-  console.log(routeProps)
-  const authed = false
+  const { routes } = routeProps;
+  console.log(routeProps);
+  const authed = false;
   return routes ? (
     <div>
       <h1>A</h1>
       {RenderRoutes(routes, authed)}
     </div>
-  ) : null
-}
+  ) : null;
+};
 
 export const RouteDemoB = (props: routeProps) => {
-  return <h1>B</h1>
-}
+  return <h1>B</h1>;
+};
